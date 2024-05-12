1fd1608577e61fdefad1c441cb9aca2fd8efac82
= <divquan@gmail.com>
2024-05-12 15:51:20 +0000

    chore: Update Navbar component to include mobile menu functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,23 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HiMenuAlt3 } from "react-icons/hi";
+import { HiXMark } from "react-icons/hi2";
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const body = document.getElementsByTagName("body");
+    console.log("body: ", body);
+    if (!body[0]) return;
+    if (isOpen) {
+      body[0].style.overflow = "hidden";
+    } else {
+      body[0].style.overflow = "visible";
+    }
+  }, [isOpen]);
   return (
     <div className="w-full flex justify-center items-center  px-4 md:px-6 py-2 md:py-3">
       <div className="flex justify-between items-center max-w-6xl  w-full">
@@ -26,9 +38,32 @@ const Navbar = () => {
           ))}
         </div>
         <div className="md:hidden">
-          <HiMenuAlt3 size={32} />
+          <HiMenuAlt3 size={32} onClick={() => setIsOpen(true)} />
         </div>
       </div>
+      {isOpen && (
+        <div className="bg-black bg-opacity-50 top-0 left-0 h-screen w-screen fixed z-50 ">
+          <div
+            className={`bg-white  h-full flex flex-col justify-center items-center gap-5`}
+          >
+            {NavbarLinks.map((link) => (
+              <Link
+                key={link.name}
+                href={link.href}
+                className={`text-lg md:text-2xl font-semibold ${
+                  pathname === link.href ? "text-qgreen" : "text-black"
+                }`}
+                onClick={() => setIsOpen(false)}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+          <div className="absolute top-0 right-0 p-4">
+            <HiXMark size={32} onClick={() => setIsOpen(false)} />
+          </div>
+        </div>
+      )}{" "}
     </div>
   );
 };
